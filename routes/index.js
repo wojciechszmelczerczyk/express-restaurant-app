ec3b22bdfce690b59f4c16189513d035c05ca66f
Wojciech <szmelczerczykwojciech@gmail.com>
2021-08-13 22:40:15 +0200

    changes in views directory, some changes in routing

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
 const {
-    MongoClient
-} = require('mongodb');
+    request
+} = require('http');
 const app = express();
 
 
@@ -15,11 +15,21 @@ function wrapper(app) {
         console.log("Server running at:http://127.0.0.1:3000");
     });
 
+    //template engine
+    app.set('template engine', 'ejs');
+    // app.set('view engine', 'ejs'); //whats the difference between line above and this?
+
     //malware for static files
     app.use('/static', express.static('public'));
 
-    //template engine
-    app.set('template engine', 'ejs');
+    app.use(express.json());
+    app.use(express.urlencoded({
+        extended: true
+    }));
+
+
+
+
 
     //routing
     app.get('/', (req, res) => {
@@ -43,11 +53,93 @@ function wrapper(app) {
                     if (err) {
                         console.log(err);
                     } else {
-                        console.log(items);
+                        // console.log(items);
                         res.render(path.join(__dirname, '../public/views', 'index.ejs'), {
-                            name: [items[0].name, items[1].name],
-                            price: [items[0].price, items[1].price],
-                            description: [items[0].description, items[1].description]
+                            name: [items[0].name, items[1].name, items[2].name, items[3].name],
+                            price: [items[0].price, items[1].price, items[2].price, items[3].price],
+                            description: [items[0].description, items[1].description, items[2].description, items[3].description]
+
+                        });
+                    }
+                });
+
+
+
+            }
+
+        });
+        // client.close(); //something wrong with ending connection 
+
+    })
+
+
+
+    app.get('/menu', (req, res) => {
+        const mongo = require('mongodb');
+        const client = new mongo.MongoClient('mongodb://localhost:27017', { // connect to mongodb
+            useNewUrlParser: true
+        });
+
+        client.connect((err) => { // connect to mongodb
+            if (err) {
+                console.log(err); // if error, print it
+            } else {
+                console.log('Connected to the database'); // connected to the database
+
+                const db = client.db('MammaMia'); // get db
+
+                const menu = db.collection('menu'); // collection
+
+                menu.find({}).toArray((err, items) => { // find all items
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(items);
+                        res.render(path.join(__dirname, '../public/views', 'menu.ejs'), {
+                            name: [items[0].name, items[1].name, items[2].name, items[3].name, items[4].name, items[5].name, items[6].name, items[7].name],
+                            // name: items.forEach((item, i) => item[i].name), //optimalize this shit
+                            price: [items[0].price, items[1].price, items[2].price, items[3].price, items[4].price, items[5].price, items[6].price, items[7].price],
+                            description: [items[0].description, items[1].description, items[2].description, items[3].description, items[4].description, items[5].description, items[6].description, items[7].description]
+
+                        });
+                    }
+                });
+
+
+
+            }
+
+
+        });
+
+
+    })
+
+
+
+    app.get('/gallery', (req, res) => {
+        const mongo = require('mongodb');
+        const client = new mongo.MongoClient('mongodb://localhost:27017', { // connect to mongodb
+            useNewUrlParser: true
+        });
+
+        client.connect((err) => { // connect to mongodb
+            if (err) {
+                console.log(err); // if error, print it
+            } else {
+                console.log('Connected to the database'); // connected to the database
+
+                const db = client.db('MammaMia'); // get db
+
+                const gallery = db.collection('gallery'); // collection
+
+                gallery.find({}).toArray((err, items) => { // find all items
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        // console.log(items);
+                        res.render(path.join(__dirname, '../public/views', 'gallery.ejs'), {
+                            photo: items[0].url,
 
                         });
                     }
@@ -59,19 +151,7 @@ function wrapper(app) {
             // client.close(); //something wrong with ending connection 
 
         });
-    })
 
-
-
-    app.get('/menu', (req, res) => {
-        res.render(path.join(__dirname, '../public/views', 'menu.ejs'));
-
-    })
-
-
-
-    app.get('/gallery', (req, res) => {
-        res.render(path.join(__dirname, '../public/views', 'gallery.ejs'));
     })
 
 
@@ -98,7 +178,7 @@ function wrapper(app) {
                     } else {
                         // console.log(items);
                         res.render(path.join(__dirname, '../public/views', 'order.ejs'), {
-                            name: [items[0].name, items[1].name],
+                            name: [items[0].name, items[1].name, items[2].name, items[3].name, items[4].name, items[5].name, items[6].name, items[7].name],
 
                         });
                     }
@@ -114,15 +194,54 @@ function wrapper(app) {
     })
 
     app.get('/your-order', (req, res) => {
-        res.render(path.join(__dirname, '../public/views', 'your-order.ejs'));
+        res.render(path.join(__dirname, '../public/views', 'your-order.ejs'))
     })
+
+
+
 
     app.post('/your-order', (req, res) => {
-        console.log(req.query);
-        res.send('POST request to homepage')
+        const mongo = require('mongodb');
+        const client = new mongo.MongoClient('mongodb://localhost:27017', { // connect to mongodb
+            useNewUrlParser: true
+        });
 
-        // res.redirect('/order');
+        client.connect((err) => { // connect to mongodb
+            if (err) {
+                console.log(err); // if error, print it
+            } else {
+                console.log('Connected to the database'); // connected to the database
+
+                const db = client.db('MammaMia'); // get db
+
+                const order = db.collection('order'); // collection
+
+                order.insertOne({
+                    name: req.body.from
+                }) // insert one element
+                // console.log(items);
+                res.render(path.join(__dirname, '../public/views', 'your-order.ejs'), {
+                    from: req.body.from,
+                    to: req.body.to
+                });
+
+
+
+
+
+            }
+            // client.close(); //something wrong with ending connection 
+
+        });
+
+
+
     })
+
+
+
+
+
 
     app.get('/about', (req, res) => {
         res.render(path.join(__dirname, '../public/views', 'about.ejs'));
